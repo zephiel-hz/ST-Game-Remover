418b5540d9b3fe9b01147d0fd5971aff59154342
Vamola <aj8673074@gmail.com>
2026-08-11 22:38:50 +0700

    Remove database SQL scripts from repository

--- Database/add_storage_fields_to_hzmanifest_files.sql
+++
@@ -1,8 +0,0 @@
--- Add storage metadata fields to hzmanifest_files for folder-based storage support
-ALTER TABLE hzmanifest_files
-ADD COLUMN IF NOT EXISTS storage_type TEXT DEFAULT 'zip';
-
-ALTER TABLE hzmanifest_files
-ADD COLUMN IF NOT EXISTS folder_path TEXT;
-
-CREATE INDEX IF NOT EXISTS idx_hzmanifest_files_storage_type ON hzmanifest_files(storage_type);
